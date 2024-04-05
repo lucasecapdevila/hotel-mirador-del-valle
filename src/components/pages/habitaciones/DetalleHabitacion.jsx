318db6e50e85427ea48c6e44524429059fa85a1f
Natalia Lopez <natigab.24@gmail.com>
2024-04-05 20:25:46 -0300

    agrego estilos y completo con los datos

--- src/components/pages/habitaciones/DetalleHabitacion.jsx
+++ src/components/pages/habitaciones/DetalleHabitacion.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { obtenerHabitacionAPI } from "../../../helpers/queries";
 import Swal from "sweetalert2";
 const DetalleHabitacion = () => {
-    const { id } = useParams();
+  const { id } = useParams();
   const [habitacion, setHabitacion] = useState({});
 
   useEffect(() => {
@@ -24,35 +24,24 @@ const DetalleHabitacion = () => {
       });
     }
   };
-    return (
-        <Container className="mainPage my-3">
+  return (
+    <Container className="mainPage my-3">
       <Row>
-      <Col md={6} className="p-0">
-          <Image src=''></Image>
-        </Col>
-        <Col md={6}>
+        <Col md={6} className="p-0">
+          <Image src={habitacion.imagenHabitacion}></Image>
           <h5 className="mt-4"></h5>
-          <p>DESCRIPCIÓN AMPLIA Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo veritatis, possimus beatae nihil, 
-            delectus asperiores ad harum aliquid cum amet praesentium similique ab cupiditate nobis illum! Voluptatum beatae ratione officia?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptates quae enim nemo. 
-            Doloribus perspiciatis ratione eveniet earum deserunt qui corrupti, et, vero quo delectus exercitationem ipsum autem recusandae! Ut.</p>
+          <p>{habitacion.descripcionAmplia}</p>
         </Col>
         <Col md={6} className="contenedorDetalleHabitacion text-center ">
-          <h1 className="display-4 fw-semibold my-5 titulos">Habitacion Doble</h1>
-          <p className="my-5 fs-5">
-            min 
-          </p>
-          <p className="fs-5"></p>
-        </Col>
-       
-        
-        <Col md={6}>
-          <h5 className="mt-4"></h5>
-          <p></p>
+          <h1 className="display-4 fw-semibold my-5 titulos">
+            {habitacion.tipoHabitacion}
+          </h1>
+          <p className="my-5 fs-5">{habitacion.precioHabitacion}</p>
+          <p className="fs-5">{habitacion.disponibilidad}</p>
         </Col>
       </Row>
     </Container>
-    );
+  );
 };
 
 export default DetalleHabitacion;
