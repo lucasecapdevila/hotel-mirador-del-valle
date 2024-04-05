cfe3bac30ab66d1984315308426149e02e020498
Natalia Lopez <natigab.24@gmail.com>
2024-04-05 20:01:46 -0300

    agrego estilos de bootstrap

--- src/components/pages/habitaciones/DetalleHabitacion.jsx
+++ src/components/pages/habitaciones/DetalleHabitacion.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import {Container, Row, Col, Image} from "react-bootstrap";
 
 const DetalleHabitacion = () => {
     return (
         <Container className="mainPage my-3">
       <Row>
         <Col md={6} className="contenedorDetalleHabitacion text-center ">
-          <h1 className="display-4 fw-semibold my-5"></h1>
+          <h1 className="display-4 fw-semibold my-5 titulos">Habitacion Doble</h1>
           <p className="my-5 fs-5">
             min 
           </p>
