863898990e79348ba97ad5232e5acfab4a69a187
Natalia Lopez <natigab.24@gmail.com>
2024-04-04 22:52:22 -0300

    creo el componente CardHabitacion

---
+++ src/components/pages/habitaciones/CardHabitacion.jsx
@@ -0,0 +1,14 @@
+import { Button, Card, Col } from "react-bootstrap";
+import imagenCard from "../../../assets/habitacion_doble.jpg";
+
+const CardHabitacion = () => {
+  return (
+    <Col md={4} className="mb-3">
+      <Card>
+        
+      </Card>
+    </Col>
+  );
+};
+
+export default CardHabitacion;
