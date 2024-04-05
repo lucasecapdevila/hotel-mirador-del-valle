fd9be527a52ce8c44ebd12b30445143acff4fe0a
Natalia Lopez <natigab.24@gmail.com>
2024-04-04 23:08:21 -0300

    creo la estructura del componente

---
+++ src/components/pages/habitaciones/CardHabitacion.jsx
@@ -0,0 +1,25 @@
+import { Button, Card, Col } from "react-bootstrap";
+import imagenCard from "../../../assets/habitacion_doble.jpg";
+
+const CardHabitacion = () => {
+  return (
+    <Col md={4} className="mb-3">
+      <Card>
+        <Card.Img
+          variant="top"
+          src={imagenCard}
+          className="card-img-top-nueva"
+        />
+        <Card.Body className="bodyCardHabitacion">
+          <Card.Title></Card.Title>
+          <Card.Text>
+           
+          </Card.Text>
+          <Button className="btnCardHabitacion"></Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default CardHabitacion;
