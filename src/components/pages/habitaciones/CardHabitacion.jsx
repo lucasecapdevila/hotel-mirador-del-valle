13bb7efb20f7606ebe98fec3c63d4b33b9a813ab
Natalia Lopez <natigab.24@gmail.com>
2024-04-04 22:53:00 -0300

    agrego los texto a modo de ejemplo

--- src/components/pages/habitaciones/CardHabitacion.jsx
+++ src/components/pages/habitaciones/CardHabitacion.jsx
@@ -5,7 +5,19 @@ const CardHabitacion = () => {
   return (
     <Col md={4} className="mb-3">
       <Card>
-        
+        <Card.Img
+          variant="top"
+          src={imagenCard}
+          className="card-img-top-nueva"
+        />
+        <Card.Body className="bodyCardHabitacion">
+          <Card.Title>HABITACION DOBLE</Card.Title>
+          <Card.Text>
+            Las habitaciones dobles poseen todo lo necesario para compartir unos
+            días de descanso en pareja,
+          </Card.Text>
+          <Button className="btnCardHabitacion">DETALLES</Button>
+        </Card.Body>
       </Card>
     </Col>
   );
