422108f5c295575dc03d2536d52bd401c5f2b8bf
Lucas Capdevila <lcapdevila60@gmail.com>
2024-04-05 15:19:32 -0300

    se agrega texto introductorio

--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -29,7 +29,22 @@ const QuienesSomos = () => {
       </Carousel>
 
       <Container>
-        asd
+        <section className='my-4'>
+          <h2 className='text-center fs-1 titulos'>Hotel Mirador del Valle</h2>
+          <article className='fs-5 textoJustificado textos'>
+            <p className='mb-4'>
+              Bienvenido al Hotel Mirador del Valle, donde la elegancia se encuentra con la naturaleza para ofrecerte una experiencia inolvidable. Ubicado en el pintoresco valle de Tafí, nuestro hotel es un refugio de tranquilidad y comodidad.
+            </p>              
+            <p className='mb-4'>
+              En Mirador del Valle, nos enorgullece brindar un servicio excepcional y una atención personalizada para satisfacer las necesidades de cada huésped. Nuestro equipo dedicado está comprometido a hacer que tu estancia sea lo más placentera posible, ofreciendo un ambiente acogedor y familiar.
+            </p>
+            <p className='mb-4'>
+              Disfruta de nuestras amplias y confortables habitaciones con vistas panorámicas que te cautivarán desde el momento en que llegues. Además, nuestras instalaciones cuentan con una variedad de servicios y comodidades diseñadas para hacer que tu visita sea inolvidable, desde exquisita gastronomía hasta actividades recreativas para todas las edades.
+            </p>
+            <p className='mb-4'>Ya sea que estés aquí por negocios o placer, en el Hotel Mirador del Valle encontrarás todo lo que necesitas para una experiencia única. ¡Esperamos darte la bienvenida pronto y convertir tu estancia en momentos inolvidables!
+            </p>
+          </article>
+        </section>
       </Container>
     </main>
   )
