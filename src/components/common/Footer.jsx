94f37e48ceb3fc8b0ee0ee648d7401899fafd8e1
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-04-05 20:18:24 -0300

    agregue el maquetado del footer, haciendolo responsive para dispositivos lg, md y sm

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,32 @@
+import { Container } from 'react-bootstrap'
+
 
 const Footer = () => {
     return (
         <>
         <footer className="text-center bg-dark text-light py-3">
-            <p>&copy; Todos Los Derechos Reservados</p>
+        <Container className='d-flex justify-content-center footerdiv'>
+        <div className="bg-primary col-sm-12 col-md-3 col-lg-3">
+        <p>Parrafo </p>
+        <p>Parrafo</p>
+        <p>Parrafo </p>
+        </div>
+        <div className="bg-primary col-sm-12 col-md-3 col-lg-3 mx-lg-3">
+        <p>Info footer</p>
+        <p>Info footer</p>
+        <a href="mailto:hotelmiradordelvalle.25.07@gmail.com" className='text-light'>hotelmiradordelvalle.25.07@gmail.com</a>
+        </div>
+        <div className="bg-primary col-sm-12 col-md-3 col-lg-3">
+        <p>Info footer</p>
+        <p>Info footer</p>
+        <p>Info footer</p>
+        </div>
+        <div className="bg-primary col-sm-12 col-md-3 col-lg-3 ms-lg-3">
+        <p>Info footer </p>
+        <p>Info footer </p>
+        <p>Info footer </p>
+        </div>
+        </Container>
         </footer>
         </>
     );
