f39628b22edd23bdd4d942dc2b788d29e2706cb2
Lucas Capdevila <lcapdevila60@gmail.com>
2024-04-06 17:08:25 -0300

    se formatea con Prettier todos los documentos

--- src/components/pages/habitaciones/CardHabitacion.jsx
+++ src/components/pages/habitaciones/CardHabitacion.jsx
@@ -1,9 +1,8 @@
 import { Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const CardHabitacion = ({habitacion}) => {
+const CardHabitacion = ({ habitacion }) => {
   return (
-    
     <Col md={4} className="mb-3">
       <Card className="my-3 cardCategoria">
         <Card.Img
@@ -12,13 +11,21 @@ const CardHabitacion = ({habitacion}) => {
           className="card-img-top-nueva"
         />
         <Card.Body className="bodyCardHabitacion d-flex flex-column">
-          <Card.Title className="titulos">{habitacion.tipoHabitacion}</Card.Title>
-          <Card.Text className="textos">{habitacion.descripcionBreve}</Card.Text>
-          <Link className="btn w-100 fw-semibold mt-auto btnCardHabitacion" to={'/detalleHabitacion/'+ habitacion.id} >DETALLES</Link>
+          <Card.Title className="titulos">
+            {habitacion.tipoHabitacion}
+          </Card.Title>
+          <Card.Text className="textos">
+            {habitacion.descripcionBreve}
+          </Card.Text>
+          <Link
+            className="btn w-100 fw-semibold mt-auto btnCardHabitacion"
+            to={"/detalleHabitacion/" + habitacion.id}
+          >
+            DETALLES
+          </Link>
         </Card.Body>
       </Card>
     </Col>
-    
   );
 };
 
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,66 +1,66 @@
-const URI_HABITACIONES = import.meta.env.VITE_API_HABITACIONES
+const URI_HABITACIONES = import.meta.env.VITE_API_HABITACIONES;
 
 //  Solicitud tipo GET (o Request) para el array de habitaciones
-export const leerHabitacionesAPI = async() => {
+export const leerHabitacionesAPI = async () => {
   try {
-    const response = await fetch(URI_HABITACIONES)
-    const listaHabitaciones = await response.json()
-    return listaHabitaciones
+    const response = await fetch(URI_HABITACIONES);
+    const listaHabitaciones = await response.json();
+    return listaHabitaciones;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 //  Solicitud tipo GET (o Request) para una única habitación
-export const obtenerHabitacionAPI = async(id) => {
+export const obtenerHabitacionAPI = async (id) => {
   try {
-    const response = await fetch(`${URI_HABITACIONES}/${id}`)
-    return response
+    const response = await fetch(`${URI_HABITACIONES}/${id}`);
+    return response;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 //  Solicitud tipo POST (crear)
-export const crearHabitacionAPI = async(nuevaHabitacion) => {
+export const crearHabitacionAPI = async (nuevaHabitacion) => {
   try {
     const response = await fetch(URI_HABITACIONES, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(nuevaHabitacion)
-    })
-    return response
+      body: JSON.stringify(nuevaHabitacion),
+    });
+    return response;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 //  Solicitud tipo PUT (editar)
-export const editarHabitacionAPI = async(id, habitacion) => {
+export const editarHabitacionAPI = async (id, habitacion) => {
   try {
-    const response = await fetch(`${URI_HABITACIONES}/${id}`,{
+    const response = await fetch(`${URI_HABITACIONES}/${id}`, {
       method: "PUT",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(habitacion)
-    })
-    return response
+      body: JSON.stringify(habitacion),
+    });
+    return response;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 //  Solicitud tipo DELETE (eliminar)
-export const borrarHabitacionAPI = async(id) => {
+export const borrarHabitacionAPI = async (id) => {
   try {
-    const response = await fetch(`${URI_HABITACIONES}/${id}`,{
-      method: "DELETE"
-    })
-    return response
+    const response = await fetch(`${URI_HABITACIONES}/${id}`, {
+      method: "DELETE",
+    });
+    return response;
   } catch (error) {
     console.log(error);
   }
-}
+};
