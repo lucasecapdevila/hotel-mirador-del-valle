bd4cb38292dd73b50daa36f88bc3dc6ac9350fa1
Lucas Capdevila <lcapdevila60@gmail.com>
2024-04-13 19:12:39 -0300

    se ajusta componente Habitaciones para agregar librerías necesarias

--- src/components/pages/Habitaciones.jsx
+++ src/components/pages/Habitaciones.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import { ConfigProvider, DatePicker, Space } from "antd";
+import locale from "antd/locale/es_ES";
+import dayjs from "dayjs";
+import "dayjs/locale/es";
 import { Container, Row } from "react-bootstrap";
 import CardHabitacion from "./habitaciones/CardHabitacion";
 import { leerHabitacionesAPI } from "../../helpers/queries";
+
 const Habitaciones = () => {
   const [listaHabitaciones, setListaHabitaciones] = useState([]);
+  const [fechaEntrada, setFechaEntrada] = useState()
+  const [fechaSalida, setFechaSalida] = useState()
 
   useEffect(() => {
     traerHabitaciones();
   }, []);
+  
+  const { RangePicker } = DatePicker;
+  dayjs.locale("es");
 
   const traerHabitaciones = async () => {
     try {
@@ -17,8 +27,14 @@ const Habitaciones = () => {
       console.log(error);
     }
   };
+
+  const filtrarPorFecha = (fechas) => {
+    setFechaEntrada(dayjs(fechas[0]).format("DD-MM-YYYY"))
+    setFechaSalida(dayjs(fechas[1]).format("DD-MM-YYYY"))
+  };
+
   return (
-    <>
+    <ConfigProvider locale={locale}>
       <div className="portadaHabitaciones mb-5 text-center">
         <img
           className="banner"
@@ -28,14 +44,17 @@ const Habitaciones = () => {
           <h1 className="">HABITACIONES</h1>
         </div>
       </div>
-      <Container>
+      <Container className="mainPage">
+        <Space direction="vertical" size={12}>
+          <RangePicker format="DD-MM-YYYY" onChange={filtrarPorFecha} />
+        </Space>
         <Row>
           {listaHabitaciones.map((habitacion) => (
-            <CardHabitacion key={habitacion.id} habitacion={habitacion} />
+            <CardHabitacion key={habitacion.id} habitacion={habitacion} fechaEntrada={fechaEntrada} fechaSalida={fechaSalida} />
           ))}
         </Row>
       </Container>
-    </>
+    </ConfigProvider>
   );
 };
 
