f45e62f5ee9fa51b5f283dd82dff3fea605c87af
Lucas Capdevila <lcapdevila60@gmail.com>
2024-04-14 15:18:18 -0300

    se agrega variable para calcular precio total de la reserva

--- src/components/pages/reservas/CardReserva.jsx
+++ src/components/pages/reservas/CardReserva.jsx
@@ -13,7 +13,7 @@ const CardReserva = ({id, fechaEntrada, fechaSalida}) => {
   const fecha1 = dayjs(fechaEntrada, "DD-MM-YYYY")
   const fecha2 = dayjs(fechaSalida, "DD-MM-YYYY")
   const diasTotales = fecha2.diff(fecha1, 'day') + 1
-  
+  const precioTotal = diasTotales * habitacion.precioHabitacion
 
   const cargarDetalle = async () => {
     const respuesta = await obtenerHabitacionAPI(id);
@@ -33,7 +33,7 @@ const CardReserva = ({id, fechaEntrada, fechaSalida}) => {
     <Col className="mb-3 mx-2 mx-md-0">
       <Card className="my-3">
         <CardHeader>
-          <Card.Title className="text-center text-md-start m-0 titulos">
+          <Card.Title className="text-center text-md-start my-2 titulos">
             <h4>{habitacion.tipoHabitacion}</h4>
           </Card.Title>
         </CardHeader>
@@ -62,7 +62,7 @@ const CardReserva = ({id, fechaEntrada, fechaSalida}) => {
                 <ul className="list-unstyled">
                   <li className="fs-5 textos mb-2">Total de días: <span className="textosAlternativos">{diasTotales}</span></li>
                   <li className="fs-5 textos mb-2">Precio por día: <span className="textosAlternativos">${habitacion.precioHabitacion}</span></li>
-                  <li className="fs-5 textos mb-2">Precio total: <span className="textosAlternativos">Precio * cantidad de días</span></li>
+                  <li className="fs-5 textos mb-2">Precio total: <span className="textosAlternativos">${precioTotal}</span></li>
                 </ul>
               </div>
               <Button className="w-100 m-auto ms-1 mb-0">Pagar ahora</Button>
