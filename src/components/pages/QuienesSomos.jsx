f1394e10f3ce4c28783f3262d15220a9ee65c528
Lucas Capdevila <lcapdevila60@gmail.com>
2024-04-21 01:37:58 -0300

    se agrega sección 'Historia del hotel'

--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -1,20 +1,4 @@
-import {
-  faBriefcase,
-  faDumbbell,
-  faFaceSmile,
-  faUtensils,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import {
-  Accordion,
-  Carousel,
-  Container,
-  Row,
-  Col,
-  Button,
-  Modal,
-} from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
 
 const QuienesSomos = () => {
   return (
@@ -85,6 +69,67 @@ const QuienesSomos = () => {
               única. ¡Esperamos darte la bienvenida pronto y convertir tu
               estancia en momentos inolvidables!
             </p>
+          </article>
+        </section>
+
+        <section className="my-5">
+          <h2 className="text-center fs-1 titulos">Nuestra historia</h2>
+          <article className="fs-5 my-4 textoJustificado textos">
+            <div className="d-flex flex-column flex-xl-row justify-content-center align-items-start">
+              <img
+                className="img-fluid mb-4 me-xl-4 imgEntradaHotel"
+                src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/243247791.jpg?k=3c07e955c37818f415ca2e8581a04c94744f46df996ae3281ed5bcf7fff45c12&o=&hp=1"
+                alt="Nuestro hotel"
+              />
+              <p className="mb-4">
+                El emblemático edificio del Hotel fue construido en dos etapas.
+                La primera en el año 1978, significó un hito en los servicios
+                hoteleros de Tafí, ya que el proyecto ofrecía características
+                arquitectónicas de singular belleza y calidez, y fue realizado
+                empleando madera y otros elementos naturales. Cada detalle del
+                diseño fue cuidadosamente considerado para integrarse
+                armoniosamente con el paisaje circundante. Las grandes aberturas
+                permitían que la luz natural inundara los espacios interiores,
+                mientras que las vistas panorámicas del Valle de Tafí se
+                convertían en una parte integral de la experiencia de hospedaje.
+                El diseño arquitectónico, con sus líneas limpias y su enfoque en
+                la simplicidad elegante, atrajo tanto a los viajeros locales
+                como a los internacionales en busca de una experiencia única y
+                en sintonía con la naturaleza.
+              </p>
+            </div>
+          </article>
+
+          <article className="fs-5 my-4 textoJustificado textos">
+            <div className="d-flex flex-column flex-xl-row-reverse justify-content-center align-items-start">
+              <img
+                className="img-fluid mb-4 ms-xl-4 imgEntradaHotel"
+                src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/6836911.jpg?k=da8b3433a2b7e003fb4640c44688eae91c890900c3ad3dec440bce5bfeb23468&o=&hp=1"
+                alt="Nuestro hotel"
+              />
+              <div>
+                <p className="mb-4">
+                  En la segunda etapa, concretada en 1986, se agregaron 20
+                  habitaciones, entre las cuales destacan las dos Suites y un
+                  amplio sector de esparcimiento con Sala de TV, Mesa de Juegos
+                  y un Living para reuniones íntimas, todo esto con ingreso
+                  directo desde el Estacionamiento semicubierto y el sector de
+                  Solárium, Quincho y Juegos Infantiles. En el año 2022 se
+                  realizó una nueva expansión que no solo se limitó a sus
+                  instalaciones físicas, sino que también se reflejó en su
+                  reputación y prestigio en la industria hotelera. Se convirtió
+                  en un punto de referencia para viajeros exigentes que buscaban
+                  una experiencia única y auténtica en el corazón de Tafí. Desde
+                  entonces, el Hotel ha seguido evolucionando y adaptándose a
+                  las cambiantes necesidades de sus huéspedes, pero su
+                  compromiso con la excelencia en servicio y hospitalidad ha
+                  permanecido inquebrantable a lo largo de los años. Hoy en día,
+                  sigue siendo un destino de ensueño para aquellos que buscan
+                  una escapada tranquila y lujosa en medio de la belleza natural
+                  del Valle de Tafí.
+                </p>
+              </div>
+            </div>
           </article>
         </section>
       </Container>
