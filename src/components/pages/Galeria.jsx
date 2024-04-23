551be73fe20809f8a106c092a17f51c3bd1626e0
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-04-23 04:55:11 -0300

    agregue atributos alt a la galeria de imagenes

--- src/components/pages/Galeria.jsx
+++ src/components/pages/Galeria.jsx
@@ -84,6 +84,7 @@ const Galeria = () => {
           <img
             className="img-fluid"
             src="https://th.bing.com/th/id/R.5b37faf8cc966e41e8465b589b639c74?rik=YcRS4yQ6ICHHfQ&pid=ImgRaw&r=0"
+            alt="imagen de vista de tafi del valle, con catus y un hermoso paisaje"
           />
           <Carousel.Caption>
             <h2 className="fs-1 text-uppercase titulos">GALERIA DE IMAGENES</h2>
@@ -94,7 +95,8 @@ const Galeria = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img src="https://th.bing.com/th/id/R.9c52c9c4b667afb0b4c92b685978f440?rik=gMc9iQqWYIK8Bg&pid=ImgRaw&r=0" />
+          <img src="https://th.bing.com/th/id/R.9c52c9c4b667afb0b4c92b685978f440?rik=gMc9iQqWYIK8Bg&pid=ImgRaw&r=0" 
+          alt="imagen de paisaje de tafi del valle, montañas y renovacion"/>
           <Carousel.Caption>
             <h2 className="fs-1 text-uppercase titulos">Valles Calchaquies</h2>
             <hr />
@@ -110,10 +112,10 @@ const Galeria = () => {
         <Container className="d-flex galeriaIMG">
           <article className="col-lg-3 mb-1 ">
             <a href="#!" onClick={() => img1Show(true)}>
-              <img
+              <img 
                 className="w-100 mb-2 rounded"
                 src="https://webbox.imgix.net/images/tgvirijswhqazimw/15f2acc4-6011-42ed-9306-a55780d38ef8.jpeg?auto=format,compress&fit=crop&crop=entropy&w=1318"
-                alt="habitacion del hotel doble"
+                alt="habitacion del hotel doble, con dos camas matrimoniales"
               />
             </a>
           </article>
@@ -122,7 +124,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://th.bing.com/th/id/R.da812afc5e9c5249d36e06f19200601e?rik=Yang08J6h2qevg&riu=http%3a%2f%2fwww.hotelalmirante.com%2fwp-content%2fuploads%2f2016%2f09%2falmirante_alta_editadas-9160.jpg&ehk=9x%2b2pHEqZlVtei4ydS3QIPSVyuHISRi22UWP2FeNcXw%3d&risl=&pid=ImgRaw&r=0"
-                alt="habitacion del hotel deluxe"
+                alt="habitacion del hotel deluxe, con tres camas individuales ideal para pasar con amigos"
               />
             </a>
           </article>
@@ -131,7 +133,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://www.wyndhampolanco.com/img/galeria/HABITACIONES/2020/WHYNDHAM-POLANCO-HAB-DOB-A.jpg"
-                alt="habitaciones suite"
+                alt="habitaciones suite, con la mejor calidad de dos camas y un televisor de 45 pulgadas"
               />
             </a>
           </article>
@@ -140,7 +142,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://i.pinimg.com/originals/4d/64/67/4d64679bfd445492de4e7ed92cb66873.jpg"
-                alt="habitaciones twin"
+                alt="habitaciones twin, individuales para amigos"
               />
             </a>
           </article>
@@ -154,7 +156,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://images.pexels.com/photos/1449773/pexels-photo-1449773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="restaurante del hotel"
+                alt="restaurante del hotel, para pasar todos tus ratos"
               />
             </a>
           </article>
@@ -163,7 +165,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://images.pexels.com/photos/6796964/pexels-photo-6796964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="gimnasio"
+                alt="gimnasio del hotel, con maquinas, pesas y todo lo necesario que tiene un gimnasio"
               />
             </a>
           </article>
@@ -172,7 +174,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://images.pexels.com/photos/462129/pexels-photo-462129.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="piscina"
+                alt="piscina grande del hotel donde se reflejan palmeras de fondo y sillones para tomar sol"
               />
             </a>
           </article>
@@ -181,7 +183,7 @@ const Galeria = () => {
               <img
                 className="w-100 mb-2 rounded"
                 src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/47/c6/b5/hotel-waynay-killa.jpg?w=700&h=-1&s=1"
-                alt="valles"
+                alt="balcon del hotel donde se ve una hermosa vista a los valles calchaquies"
               />
             </a>
           </article>
@@ -243,7 +245,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://webbox.imgix.net/images/tgvirijswhqazimw/15f2acc4-6011-42ed-9306-a55780d38ef8.jpeg?auto=format,compress&fit=crop&crop=entropy&w=1318"
-                alt="habitacion del hotel doble"
+                alt="habitacion del hotel doble, con camas matrimoniales y decoradas con un cuadro"
               />
               <Carousel.Caption>
                 <p>Conoce nuestras Habitaciones!!</p>
@@ -252,7 +254,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://webbox.imgix.net/images/tgvirijswhqazimw/82bf24d5-0e27-48b0-9deb-edbbdf07f056.jpeg?auto=format,compress&fit=crop&crop=entropy&w=1318"
-                alt="habitacion del hotel doble"
+                alt="habitacion del hotel doble, con camas matrimoniales"
               />
               <Carousel.Caption>
                 <p>Habitaciones Dobles</p>
@@ -261,7 +263,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://webbox.imgix.net/images/tgvirijswhqazimw/26591600-99f5-45e2-a3e1-ba09b85f6a5a.jpeg?auto=format,compress&fit=crop&crop=entropy&w=1318"
-                alt="habitacion del hotel doble"
+                alt="habitacion del hotel doble, con television mediana"
               />
               <Carousel.Caption>
                 <p>Comodidad y Elegancia</p>
@@ -286,7 +288,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/R.da812afc5e9c5249d36e06f19200601e?rik=Yang08J6h2qevg&riu=http%3a%2f%2fwww.hotelalmirante.com%2fwp-content%2fuploads%2f2016%2f09%2falmirante_alta_editadas-9160.jpg&ehk=9x%2b2pHEqZlVtei4ydS3QIPSVyuHISRi22UWP2FeNcXw%3d&risl=&pid=ImgRaw&r=0"
-                alt="habitacion del hotel deluxe"
+                alt="habitacion del hotel deluxe, para pasar con amigos"
               />
               <Carousel.Caption>
                 <p>Para pasar con amigos!</p>
@@ -295,7 +297,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://cdn2.paraty.es/ohtels-villaromana/images/a80bc241bb565f4=s1900"
-                alt="habitacion del hotel deluxe"
+                alt="habitacion del hotel deluxe, tres camas, tele pequeña, vista al paisaje"
               />
               <Carousel.Caption>
                 <p>Sentite como en casa!</p>
@@ -304,7 +306,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/OIP.kwZlfVoiOhpAvDaf_qlN5QHaE8?rs=1&pid=ImgDetMain"
-                alt="habitacion del hotel deluxe"
+                alt="habitacion del hotel deluxe, cuatro camas con armario"
               />
               <Carousel.Caption>
                 <p>Con las buenas compañias</p>
@@ -329,7 +331,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://www.wyndhampolanco.com/img/galeria/HABITACIONES/2020/WHYNDHAM-POLANCO-HAB-DOB-A.jpg"
-                alt="habitacion del hotel suite"
+                alt="habitacion del hotel suite, dos camas y televisor grande con vista al balcon"
               />
               <Carousel.Caption>
                 <p>Suite</p>
@@ -338,7 +340,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/OIP.WliN4IBfnzkH0gRUeJ-6EAHaE8?rs=1&pid=ImgDetMain"
-                alt="habitacion del hotel suite"
+                alt="habitacion del hotel suite, elegante, dos camas y tv"
               />
               <Carousel.Caption>
                 <p>La mejor Comodidad</p>
@@ -347,7 +349,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://www.wyndhampolanco.com/img/galeria/HABITACIONES/2020/WHYNDHAM-POLANCO-WC2.jpg"
-                alt="habitacion del hotel suite"
+                alt="habitacion del hotel suite, baño limpio, comodo y cuidado"
               />
               <Carousel.Caption>
                 <p>La mas solicitada</p>
@@ -372,7 +374,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://i.pinimg.com/originals/4d/64/67/4d64679bfd445492de4e7ed92cb66873.jpg"
-                alt="habitacion del hotel twin"
+                alt="habitacion del hotel twin, para sentirse como en casa"
               />
               <Carousel.Caption>
                 <p>Twins</p>
@@ -381,7 +383,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://i.pinimg.com/originals/06/1e/04/061e04e5dd074a322ed3ff79fd84fece.jpg"
-                alt="habitacion del hotel twin"
+                alt="habitacion del hotel twin, tres camas, con tv"
               />
               <Carousel.Caption>
                 <p>Disfruten su estancia!</p>
@@ -390,7 +392,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/0d/4f/d6/mision-aguascalientes.jpg?w=400&h=300&s=1"
-                alt="habitacion del hotel twin"
+                alt="habitacion del hotel twin, dos camas, comoda"
               />
               <Carousel.Caption>
                 <p>Oasis de descanso</p>
@@ -415,7 +417,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://hotelmiradordeltafi.com.ar/images/gallery/restaurante.jpg"
-                alt="restaurante del hotelhabitacion del hotel twin"
+                alt="restaurante del hotel"
               />
               <Carousel.Caption>
                 <p>Restaurante</p>
@@ -424,7 +426,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/14/b4/62/mirador-del-tafi-hotel.jpg?w=1400&h=-1&s=1"
-                alt="servicio de comidas hotel"
+                alt="servicio de comidas hotel, todo tipos de comidas para la mañana, tarde y noche"
               />
               <Carousel.Caption>
                 <p>Servicio a la habitacion</p>
@@ -449,7 +451,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/R.d76e6e87395e064af655ec38f4ad2d44?rik=5%2fXcTmQzw7MibQ&riu=http%3a%2f%2fwww.thesukosol.com%2fwp-content%2fuploads%2f2018%2f02%2factivity1.jpg&ehk=dCmzG6XU5Y9Gif%2bBzCiHIYFjltUNC7Va%2f4XodNuq2LA%3d&risl=&pid=ImgRaw&r=0"
-                alt="gym del hotel"
+                alt="gym del hotel, maquinas caminadoras, pesas"
               />
               <Carousel.Caption>
                 <p>Mantenete en forma en tus vacaciones!</p>
@@ -458,7 +460,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/OIP.G93O_vD_otvtKIiMCAYeXQHaEK?w=768&h=432&rs=1&pid=ImgDetMain"
-                alt="gym del hotel"
+                alt="gym del hotel, maquinas caminadoras"
               />
               <Carousel.Caption>
                 <p>Totalmente gratuito</p>
@@ -483,7 +485,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1e/19/a1/ca/hotel-colonial-tafi-del.jpg?w=1400&h=-1&s=1"
-                alt="psicina"
+                alt="piscina con vista al valle del hotel"
               />
               <Carousel.Caption>
                 <p>Divertite!</p>
@@ -492,7 +494,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/OIP.UG4YBcBqf0RusKMM5-YMWAHaFj?w=800&h=600&rs=1&pid=ImgDetMain"
-                alt="piscina"
+                alt="piscina con hermosa vista al valle"
               />
               <Carousel.Caption>
                 <p>Refrescate con una hermosa vista!</p>
@@ -517,7 +519,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://conocedores.com/wp-content/uploads/2021/09/valles-calchaquies-tucuman-30092021in3.jpg"
-                alt="valles"
+                alt="valles calchaquies de noche, con las estrellas iluminando"
               />
               <Carousel.Caption>
                 <p>Disfruta de las vistas</p>
@@ -526,7 +528,7 @@ const Galeria = () => {
             <Carousel.Item>
               <img
                 src="https://th.bing.com/th/id/OIP.O8ME_2kNgKyInsk0ueNLaAHaE7?rs=1&pid=ImgDetMain"
-                alt="tafi del valle"
+                alt="tafi del valle camino que te lleva al cerro"
               />
               <Carousel.Caption>
                 <p>De nuestro hermoso Tucuman</p>
